38730e84a92bf60ab1b5c25246439c4ef0718825
Hirak Chatterjee <hirak.chatterjee@SW-LP06894.local>
2022-12-12 23:12:44 +0530

    module changes

---
+++ cmd/runner.go
@@ -0,0 +1,11 @@
+package main
+
+//import "github.com/hirakc/learn-go-with-tests/hello"
+import (
+	"fmt"
+
+	"github.com/hirakc/learn-go-with-tests/hello"
+)
+func main() {
+	fmt.Println(hello.Hello("World", ""))
+}
